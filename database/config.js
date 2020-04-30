20d2c4b83f6b41161fd538173b92ecf8a4473161
Qasem Mohammad <https://online-shop2200.herokuapp.com/>
2020-04-30 21:15:53 +0300

    first push

---
+++ database/config.js
@@ -0,0 +1,9 @@
+let config = {
+    host:'localhost',
+        user:'root',
+        password:'12345678',
+        database:'proof'
+  };
+  
+  module.exports = config;
+   
---
+++ senddatatodatabase.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const data = require('./database/databaseConfig')
+
+
+    app.post('/form', (req, res) => {
+        let username=req.body.username;
+        let phone=req.body.phone;
+        let case =req.body.case;
+        let proof=req.body.proof;
+        let dis=req.body.dis;
+        let block=req.body.block;
+        let home=req.body.home;
+        let reg=req.body.reg;
+        console.log(username);
+    })
