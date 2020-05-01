0c71e0166c5e63c49f5cb3e85520842a495120c9
hamza732387 <hearthard40@gmail.com>
2020-05-01 20:45:50 +0300

    fuck you

--- database/config.js
+++ database/config.js
@@ -1,9 +1,25 @@
-let config = {
-    host:'localhost',
-        user:'root',
-        password:'12345678',
-        database:'proof'
-  };
   
-  module.exports = config;
+const mysql = require('mysql');
+const DB_NAME = 'complent';
+
+function createDatabaseConnection(callback) {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '12345',
+        database: DB_NAME,
+    });
+
+    connection.connect(error => {
+        if (callback) {
+            callback(error, connection)
+        }
+    });
+}
+
+module.exports = {
+    createDatabaseConnection,
+    DB_NAME
+};
+
    
