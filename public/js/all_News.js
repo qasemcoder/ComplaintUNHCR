b70c429b62e3f44ef197819f69db8cc1ce506e33
qasemcoder <qasemcoder2020@gmail.com>
2020-05-11 14:51:04 +0300

    fuck

--- public/js/all_News.js
+++ public/js/all_News.js
@@ -1,4 +1,5 @@
 let content = document.getElementById('mar')
+let del = document.getElementsByClassName('del')
 let URL = 'http://localhost:3000/'
 get_all_News()
 
@@ -10,21 +11,32 @@ function get_all_News() {
     }).then(data => {
         console.log(data)
         for (let i = 0; i < data.length; i++) {
-            content.innerHTML += `<div id="divMarq">${data[i].news}` +`<img src="./useImage/del.png" id="imgDel" title="Delete" width=20 height=20 onclick='ale();'>
-            </div> `
-            localStorage.setItem('ne', data[i].id)
-            for (let i = 0; i < data.length; i++) {
-                document.getElementById('divMarq').addEventListener('click', e => {
-                    e.target.style.color = 'red'
-                    console.log(e)
-                    let id = e.target.parentElement.getAttribute('id');
-                })
-            }
+            content.innerHTML += `<div id="divMarq">${data[i].news}` + `<img src="./useImage/del.png" 
+               id="${data[i].id}" title="Delete" width=20 height=20' class = 'del imgDel'>
+                 </div> `
+
         }
+        for (let i = 0; i < data.length; i++) {
+            del[i].addEventListener('click', e => {
+                let id = e.target.getAttribute('id');
+                console.log(id)
+                delet(id)
+                localStorage.setItem('id_N', id)
+            })
+        }
+
     })
 };
 //------------------------------------
 
-function ale(){
-    alert('you cliked om Delete button')
+function delet(id) {
+    // let id = localStorage.getItem('id_N')
+    fetch(URL + 'delItem/' + id, {
+        method: 'delete'
+    }).then(re => {
+        return re.json()
+    }).then(data => {
+        console.log(data)
+    })
+    location.reload()
 }
