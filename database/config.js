33eebdc84f1dfed500c10f669afed6a4970ad749
qasemcoder <qasemcoder2020@gmail.com>
2020-06-12 19:53:37 +0300

    freeee

--- database/config.js
+++ database/config.js
@@ -1,22 +1,22 @@
   
 const mysql = require('mysql');
-const DB_NAME = 'complent'
+const DB_NAME = 'sql12346983'
 
 
 function createDatabaseConnection(callback) {
  
-    // const connection = mysql.createConnection({
-    //     host: 'sql12.freemysqlhosting.net',
-    //     user: 'sql12346983',
-    //     password: 'eWBW3gFhvS',
-    //     database: DB_NAME,
-    // });
     const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '12345',
+        host: 'sql12.freemysqlhosting.net',
+        user: 'sql12346983',
+        password: 'eWBW3gFhvS',
         database: DB_NAME,
     });
+    // const connection = mysql.createConnection({
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: '12345',
+    //     database: DB_NAME,
+    // });
     connection.connect(error => {
         if (callback) {
             callback(error, connection)
